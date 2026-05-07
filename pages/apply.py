aa1fa5ae3365dacef7952d8d3136a2b1404f2dbd
Senuri Chathurangi <155548955+SenuriBandara@users.noreply.github.com>
2026-05-08 03:53:44 +1000

    Update apply.py

--- pages/apply.py
+++ pages/apply.py
@@ -5,19 +5,25 @@ st.set_page_config(page_title="Apply Now", layout="wide")
 st.title("Apply Now")
 
 st.subheader("Personal Information")
+
 col1, col2 = st.columns(2)
-
 with col1:
-    st.text_input("First Name")
+    st.text_input("First Name *")
 with col2:
-    st.text_input("Last Name")
+    st.text_input("Last Name *")
 
-st.text_input("Email")
-st.text_input("Phone Number")
-st.selectbox("Nationality", ["Sri Lanka", "India", "Australia", "Other"])
+st.text_input("Email *")
+st.text_input("Phone Number *")
+
+st.selectbox("Nationality *", ["Sri Lanka", "India", "Australia", "Other"])
 
 st.subheader("Program Details")
-st.selectbox("Internship Type", ["Global Internship", "Short Term", "Not Sure"])
+
+st.selectbox(
+    "Internship Type",
+    ["Global Internship", "Short-Term Internship", "Internship + Cultural Program"]
+)
+
 st.text_area("Why do you want to join?")
 
 st.subheader("Upload CV")
